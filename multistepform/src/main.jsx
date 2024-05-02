e79decf32aeb301451533df9c2d939714671e392
minseok <imuchgabis@gmail.com>
2024-05-02 14:32:25 +0900

    리덕스 퍼시스트 적용

--- multistepform/src/main.jsx
+++ multistepform/src/main.jsx
@@ -5,9 +5,13 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+export const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor} />
     <BrowserRouter>
       <React.StrictMode>
         <App />
--- multistepform/src/store/index.js
+++ multistepform/src/store/index.js
@@ -1,13 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slice/userSlice";
 import cartReducer from "./slice/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
-const store = configureStore({
-  // 여러 리듀서를 자동으로 합쳐주고 미들웨어랑 데브툴즈 기능제공한다
-  reducer: {
-    user: userReducer,
-    cart: cartReducer,
-  },
+// const store = configureStore({
+//   // 여러 리듀서를 자동으로 합쳐주고 미들웨어랑 데브툴즈 기능제공한다
+//   reducer: {
+//     user: userReducer,
+//     cart: cartReducer,
+//   },
+// });
+const reducers = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage, // 로컬 스토리지에 저장
+  whitelist: ["user", "cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = configureStore({
+  reducer: persistedReducer,
+});
 export default store;
